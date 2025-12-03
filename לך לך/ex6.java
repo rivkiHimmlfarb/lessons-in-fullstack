c724271478578d6567d60bc15d4bad3313729489
rivkiHimmlfarb <rivki3776@gmail.com>
2025-12-03 16:02:09 +0200

    Delete לך לך directory

--- לך לך/ex6.java
+++
@@ -1,8 +0,0 @@
-public class ex6 {
-    public static boolean mark(int a){
-        return (a>=0&&a<=100);
-    }
-    public static void main(String[] args) {
-        System.out.println(mark(100));
-    }
-}
